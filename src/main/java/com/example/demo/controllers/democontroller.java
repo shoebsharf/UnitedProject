826150c6044c98b8076acbe5083a1a78f1eaafe3
rahulk1618 <41724941+rahulk1618@users.noreply.github.com>
2018-09-10 20:36:58 -0400

    Added home jsp

--- src/main/java/com/example/demo/controllers/democontroller.java
+++ src/main/java/com/example/demo/controllers/democontroller.java
@@ -1,4 +1,13 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
 public class democontroller {
+    @RequestMapping(value = "/")
+    public ModelAndView home(){
+        ModelAndView modelAndView=new ModelAndView("home");
+        return modelAndView;
+    }
 }
